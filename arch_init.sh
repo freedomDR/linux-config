71b470288bfb86cd705af7a927e358bd3e61bd07
freedomDR <1640145602@qq.com>
2022-07-02 12:44:40 +0000

    update nvim

--- arch_init.sh
+++ arch_init.sh
@@ -7,21 +7,30 @@ pacman -Syu neovim
 pacman -Syu git
 pacman -Syu tmux
 pacman -Syu github-cli
+pacman -Syu nodejs
+pacman -Syu npm
 
 git config --global user.email "1640145602@qq.com"
 git config --global user.name "freedomDR"
 git config --global core.editor "nvim"
-# 配置token
+# 配置git token
 gh auth login
 
 
 # https://www.ipaddress.com/ 获取ip地址 修改/etc/hosts即可
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# oh-my-zsh 
+# oh-my-zsh
 git clone https://github.com/gpakosz/.tmux.git $prefix/oh-my-tmux
 ln -s -f $prefix/oh-my-tmux/.tmux.conf ~/.tmux.conf
 cp $prefix/oh-my-tmux/.tmux.conf.local ~/.tmux.conf.local
+
+# install my vimrc (fork from amix/vimrc)
+git clone --depth=1 https://github.com/freedomDR/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+
+
 
 
 
